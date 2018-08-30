9cd5e38da28f78cc00b6ac015a6ea22ee2543ec7
Mike Hockerman <mike@hockerman.com>
2018-08-30 10:49:27 -0400

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import * as mongoose from "mongoose";
+import { arrayProp, prop, Typegoose } from "typegoose";
+
+async function Connect(): Promise<void> {
+  return new Promise<void>((res, rej) => {
+    mongoose.connect("mongodb://localhost:27017/test", {});
+    mongoose.connection.on("error", (err) => {
+      return rej(err);
+    });
+    mongoose.connection.once("open", () => {
+      return res();
+    });
+  });
+}
+
+class EmailAddress {
+  @prop()
+  public email?: string;
+}
+
+class Profile {
+  @arrayProp({ default: () => [], items: EmailAddress })
+  // @arrayProp({ items: EmailAddress, required: true })
+  // @arrayProp({ items: EmailAddress})
+  public emailAddresses?: EmailAddress[];
+}
+
+class Person extends Typegoose {
+  @prop()
+  public profile?: Profile
+}
+
+const PersonModel = new Person().getModelForClass(Person);
+
+async function Test() {
+  const p = new PersonModel();
+  p.profile = { emailAddresses: [{ email: "test@example.com" }] };
+  await p.save();
+  const p2 = await PersonModel.findOne();
+  console.log(p2);
+}
+
+Connect();
+Test();
